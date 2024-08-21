3d5e6f3009143ad85ac4f4106cbf1480f097b560
Shrey kumar gupta <117209070+shreykumar33@users.noreply.github.com>
2024-08-22 00:11:08 +0530

    utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,6 @@
 import unittest
 import requests
-from app.main import app  # Import your Flask app
+from app.main import app  
 
 class TestAverageCalculator(unittest.TestCase):
     BASE_URL = 'http://localhost:9876/numbers'
@@ -9,8 +9,8 @@ class TestAverageCalculator(unittest.TestCase):
         test_cases = {
             'p': [2, 3, 5, 7, 11],
             'f': [55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765],
-            'e': [2, 4, 6, 8],  # Adjust based on actual response
-            'r': [1, 2, 3, 4, 5]  # Adjust based on actual response
+            'e': [2, 4, 6, 8], 
+            'r': [1, 2, 3, 4, 5]  
         }
 
         for numberid, expected_numbers in test_cases.items():
@@ -23,6 +23,7 @@ class TestAverageCalculator(unittest.TestCase):
                 avg = response_json.get('avg')
 
                 expected_avg = sum(expected_numbers) / len(expected_numbers) if expected_numbers else 0
+
 
                 self.assertEqual(numbers, expected_numbers, f"Numbers for ID {numberid} do not match expected values.")
                 self.assertEqual(avg, expected_avg, f"Average for ID {numberid} does not match the expected value.")
